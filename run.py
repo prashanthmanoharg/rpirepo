038e2dd158ca7a3f4019118151495a3596628014
Prashanth Manohar G <prashanthmanohar.g@outlook.com>
2025-04-19 15:38:19 +0530

    added exp image and gui changes

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PI_PORT"))  # Retrieve PI_PORT from environment variables
+    app.run(host="0.0.0.0", port=port, debug=True)
